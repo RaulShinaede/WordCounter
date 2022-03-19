3eeae403999f314e95d650c2d890f5867b296097
RaulShinaede <82043220+RaulShinaede@users.noreply.github.com>
2022-03-19 12:00:59 -0300

    Add files

---
+++ WordCounter/MainWindow.xaml.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Windows;
+using System.Windows.Documents;
+
+namespace WordCounter {
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window {
+        private TableRowBuilder builder;
+        public MainWindow() {
+            InitializeComponent();
+            builder = new TableRowBuilder();
+        }
+
+        private void LoadFileButton_Click(object sender, RoutedEventArgs e) {
+
+            var dialogFile = new OpenFileDialog();
+            dialogFile.Filter = "txt files (*.txt)|*.txt";
+
+            Dictionary<string, int> dic = new Dictionary<string, int>();
+
+            if (dialogFile.ShowDialog().Value) {
+
+                string filePath = dialogFile.FileName;
+                var fileStream = dialogFile.OpenFile();
+
+                using (StreamReader reader = new StreamReader(fileStream)) {
+
+                    string currentLine;
+
+                    while ((currentLine = reader.ReadLine()) != null) {
+
+                        var words = currentLine.Split(' ', '\n');
+
+                        for (int i = 0; i < words.Count(); i++) {
+
+                            string currentWord = words[i];
+
+                            if (currentWord == String.Empty) continue;
+
+                            currentWord = currentWord.ToLower().Trim(
+                                ',', '.', ';', ':', '[', ']', '?', '!',
+                                '-', '\"');
+
+                            if (int.TryParse(currentWord, out int r)) continue;
+
+                            if (dic.ContainsKey(currentWord)) {
+                                dic[currentWord]++;
+                            }
+                            else {
+                                dic.Add(currentWord, 1);
+                            }
+                        }
+                    }
+                }
+
+                TableRowGroup.Rows.Clear();
+
+                int index = 1;
+
+                foreach (var item in dic.OrderByDescending(i => i.Value)) {
+                    TableRowGroup.Rows.Add(builder.Build(index, item.Key, item.Value));
+                    index++;
+                }
+            }
+        }
+    }
+}
---
+++ WordCounter/TableRowBuilder.cs
@@ -0,0 +1,46 @@
+﻿using System.Windows;
+using System.Windows.Documents;
+using System.Windows.Media;
+
+namespace WordCounter {
+    public class TableRowBuilder {
+        public TableRow Build(int id, string word, int quantity) {
+
+            var row = new TableRow();
+
+            var idParagraph = new Paragraph(new Run(id.ToString()));
+            idParagraph.TextAlignment = TextAlignment.Center;
+            idParagraph.Foreground = Brushes.White;
+
+            var idTableCell = new TableCell();
+            idTableCell.BorderBrush = Brushes.White;
+            idTableCell.BorderThickness = new Thickness(1, 1, 1 ,1);
+            idTableCell.Padding = new Thickness(3, 2, 0, 2);
+            idTableCell.Blocks.Add(idParagraph);
+
+            var wordParagraph = new Paragraph(new Run(word));
+            wordParagraph.Foreground = Brushes.White;
+            var wordTableCell = new TableCell();
+            wordTableCell.BorderBrush = Brushes.White;
+            wordTableCell.BorderThickness = new Thickness(1, 1, 1 ,1);
+            wordTableCell.Padding = new Thickness(3, 2, 0, 2);
+            wordTableCell.Blocks.Add(wordParagraph);
+
+            var quantityParagraph = new Paragraph(new Run(quantity.ToString()));
+            quantityParagraph.TextAlignment = TextAlignment.Center;
+            quantityParagraph.Foreground = Brushes.White;
+
+            var quantityTableCell = new TableCell();
+            quantityTableCell.BorderBrush = Brushes.White;
+            quantityTableCell.BorderThickness = new Thickness(1, 1, 1 ,1);
+            quantityTableCell.Padding = new Thickness(3, 2, 0, 2);
+            quantityTableCell.Blocks.Add(quantityParagraph);
+
+            row.Cells.Add(idTableCell);
+            row.Cells.Add(wordTableCell);
+            row.Cells.Add(quantityTableCell);
+
+            return row;
+        }
+    }
+}
